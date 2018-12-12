85c2c4d62e73e084f3efebe520ee0d4afb40f619
Jan Niklas Fingerle <janniklas.fingerle@victors.de>
2018-12-12 11:40:04 +0100

    Coverage-Test

---
+++ app/components/test-component.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  init() {
+    // some code, that should be found be ember-cli-code-coverage
+    this._super(...arguments);
+  }
+});
